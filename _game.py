1a9346df8e4d7bef7a66bd11f25b5bfafdec30d4
TheModdersDen <monsterspawned@gmail.com>
2022-02-22 20:29:48 -0700

    Added Window code, began work on mobility code.
    Co-authored-by: joshliddi15 <joshliddi15@users.noreply.github.com>
    Co-authored-by: Aitanatc <Aitanatc@users.noreply.github.com>

--- _game.py
+++ _game.py
@@ -18,19 +18,24 @@ class Game():
         self.player = Player()
         self.window = turtle.Screen()
         self.window.title(f"{self.gameutils.get_game_name()}:")
-        self.window.bgcolor("black")
+        self.window.bgcolor("white")
         self.window.setup(width=600, height=600)
         self.window.tracer(0)
         self.window.listen()
         self.window.onkeypress(self.quit_game, "Escape")
+        self.window.onkeypress(self.player.move_left, "a")
+        self.window.onkeypress(self.player.move_right, "d")
+        turtle.write(f"{self.gameutils.get_game_name()}", align="center", font=("Verdana", 30, "normal"))
+        sleep(5)
 
     def start_game(self):
+        self.window.bgcolor("black")
         while True:
             self.window.update()
             if self.should_quit:
                 print(f"Thanks for playing {self.gameutils.get_game_name()}!")
                 break
-        self.window.mainloop()    
+        self.window.mainloop()
 
 if __name__ == "__main__":
     game = Game()
--- game_data/player.py
+++ game_data/player.py
@@ -6,8 +6,17 @@ class Player():
     def __init__(self):
         self.player_color = "green"
         self.player_shape = "square"
-        self.player_speed = 1
+        self.player_speed = 10
         self.player_body = turtle.Turtle()
+        self.player_body.penup()
         self.player_body.color(self.player_color)
         self.player_body.shape(self.player_shape)
-        self.player_body.goto(0,0)
+        self.player_body.sety(-250)
+
+    def move_right(self):
+        self.player_body.forward(self.player_speed)
+
+    def move_left(self):
+        self.player_body.right(180)
+        self.player_body.forward(self.player_speed)
+        self.player_body.right(180)
